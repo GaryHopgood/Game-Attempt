61dc4b8d45ad8a38cf1e44026c0f7ccf72ae9107
Gary-Hopgood <hopgood.gary@gmail.com>
2018-12-02 16:16:28 +0000

    Added Ship and Fire Code into Unity

---
+++ Space Shooter/Assets/BulletScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+     
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position += new Vector3(0.0f, 0.0f, 2.0f) * 6 * Time.deltaTime;
+        if ("Bullet(Clone)" == gameObject.name)
+        { 
+        Destroy(gameObject, 2.0f);
+        }
+    }
+
+}
---
+++ Space Shooter/Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+       
+    }
+
+    float fireRate = 0.1f;
+    float nextShot = 0.0f;
+    // Update is called once per frame
+    void Update() {
+
+
+        float speed;
+        speed = 6;
+
+        transform.rotation = Quaternion.Euler(-90.0f, 270.0f, -90.0f);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1.0f, 0.0f, 0.0f) * speed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(-60, 270.0f, -90.0f);
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f) * speed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(-120, 270.0f, -90.0f);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && (Time.time > nextShot))
+        {
+            Instantiate(GameObject.Find("Bullet"), transform.position, transform.rotation);
+            nextShot = Time.time + fireRate;
+        }
+
+       
+    }
+}
