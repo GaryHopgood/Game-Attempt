d0c248dfd6f3e61f508c84e986d94ccad6f237d7
Gary-Hopgood <hopgood.gary@gmail.com>
2018-12-07 13:23:28 +0000

    Bullet changes and Destructibles
    Not sure if any of this is correct and will stay

---
+++ Space Shooter/Assets/BasicDestructible.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDestructible : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    float health = 50.0f;
+
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Bullet(Clone)")
+        {
+            Destroy(col.gameObject);
+            health -= 2;
+        }
+    }
+}
